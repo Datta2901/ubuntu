8e18ebc8439b8c54f790da119835e6b6517489b6
manikanta2901 <manikanta2901@gmail.com>
2020-04-07 18:15:48 +0530

    april long challenge unitgcd problem solution

--- codingChallenges/c/longChallenges/april/unitG.cpp
+++ codingChallenges/c/longChallenges/april/unitG.cpp
@@ -5,31 +5,32 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t,n ;
+    int t ;
     cin>>t ;
-    int arr[t];
-    int c=t ;
-    while(t--){
-        cin>>arr[t] ;
-   }
-   for(int i=c-1;i>=0;i--){
-       n=arr[i] ;
+    while(t--)
+    {
+
+        int n ;
+        cin>>n ;
         if (n == 1) {
             cout << 1 << endl;
+
+
+
             cout<< 1 << " " << 1 <<endl;
             continue;
         }
         cout<<n/2<<endl ;
         
         if ( n%2 != 0) {
-            cout << 3 << " "<< 1 << " "<< 2 << " "<< n << endl;
+            cout <<3<< " "<<1<< " "<<2<< " "<<n<<endl;
         } else {
-            cout << 2 << " "<< 1 << " "<< 2 << endl;
+            cout <<2<< " "<<1<< " "<<2<<endl;
         }
         for (int i = 4; i <= n; i+=2){
-            cout << 2 << " " << i-1 << " " << i << endl;
+            cout <<2<< " " <<i-1<< " "<<i <<endl;
         }
-    
-    }
-    return 0 ;
+        }
+
+return 0 ;
 }           
--- codingChallenges/c/longChallenges/april/unitGcd.cpp
+++ codingChallenges/c/longChallenges/april/unitGcd.cpp
@@ -1,51 +1,32 @@
 #include<iostream>
+#include <bits/stdc++.h>
 using namespace std ;
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t ;
-    cin>>t ;
+    scanf("%d",&t) ;
     while(t--)
     {
-        int n,flag=0 ;
-        cin>>n ;
-        cout<<n/2<<endl ;
-        for(int i=1;i<=n;i+=2)
-        {
-            if(flag==0)
-            {
-                if(n%2!=0)
-                    {cout<<i<<" "<<i+1<<" "<<n<<endl;
-                    flag=1 ;
-                }
-                else
-                {  if(i==n)
-                    {break ;}
-                     if(i+1<n)
-                    { 
-                         cout<<i<<" "<<i+1<<endl;
-                    }
-                    else
-                    {
-                        cout<<i<<endl ;
-                    }
-                    flag=0 ;
-                }
-                
-            }
-            else
-            {
-                if(i+1<n)
-                    { 
-                         cout<<i<<" "<<i+1<<endl;
-                    }
-                    else
-                    {
-                        cout<<i<<endl ;
-                    }
-            }
-            
-            
+
+        int n ;
+        scanf("%d",&n);
+        if (n == 1) {
+            printf("1\n1 1\n") ;
+            continue;
+        }
+        printf("%d\n",n/2) ;
+        
+        if ( n%2 != 0) {
+            printf("3 1 2 %d\n",n) ;
+        } else {
+            printf("2 1 2\n") ;
+        }
+        for (int i = 4; i <= n; i+=2){
+           
+            printf("2 %d %d\n",i-1,i);
         }
     }
-    return 0 ;
-}
+return 0 ;
+}           
