2578daf7ed4c92c8ed2761bba568d1f9d2e9f6ad
manikanta2901 <manikanta2901@gmail.com>
2020-09-20 18:56:21 +0530

    FO practice

---
+++ SRM/elab/OOPS/functionOverloading/SaravanaStores.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+
+class Salary{
+    public:
+        void Increment(int currsal){
+           cout << currsal << endl;
+        }
+        void Increment(int currsal,int bonus){
+            cout <<  currsal + bonus << endl;
+        }
+};
+
+int main(){
+    Salary ob;
+    int a,b;
+    cin >> a >> b;
+    ob.Increment(a);
+    ob.Increment(a,b);
+    return 0;
+}
