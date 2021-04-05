a934202b826d4666c73fbcf4e28afa97e906e3ad
manikanta2901 <manikanta2901@gmail.com>
2021-04-05 19:12:34 +0530

    History is included

---
+++ .history/random/sum_20210404103535.cpp
@@ -0,0 +1,38 @@
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int a,b;
+//     cin >> a >> b;
+//     string arr[10]  = {"0","one","two","three","four","five","six","seven","eight","nine"};
+//     for(int i = a; i <= b; i++){
+//         if(i < 10){
+//             cout << arr[i] << endl;
+//         }else if(i % 2 == 0){
+//             cout << "even" << endl;
+//         }else{
+//             cout << "odd" << endl;
+//         }
+//     }
+//     return 0;
+// }
+
+#include <iostream>
+#include<bit
+using namespace std;
+  
+int squareSum(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+        sum += (2 * i) * (2 * i);
+    return sum;
+}
+  
+// Driver Code
+int main()
+{
+    cout <<  (pow(3,250) % 7)
+    return 0;
+}
