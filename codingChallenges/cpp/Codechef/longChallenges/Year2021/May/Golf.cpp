39e90722a79f010a6f297b158a17d25d77370494
manikanta2901 <manikanta2901@gmail.com>
2021-05-18 21:18:10 +0530

    may long challenges solutions added

---
+++ codingChallenges/cpp/Codechef/longChallenges/Year2021/May/Golf.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int n,x,k;
+        cin >> n >> x >> k;
+        n += 1;
+        // forward direction
+        if(x % k == 0){
+            cout << "YES\n";
+        }
+        // backward direction
+        else if((x - n) % (-k) == 0 && (x - n) / (-k) >= 0){
+            cout << "YES\n";
+        }
+        //otherwise
+        else{
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
+
+
+
+
