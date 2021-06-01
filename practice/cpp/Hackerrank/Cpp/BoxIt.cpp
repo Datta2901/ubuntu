891bfd5785dcb24f6e64eed1aa022f7b4725e6ec
manikanta2901 <manikanta2901@gmail.com>
2021-06-01 11:47:55 +0530

    Practice problems added

--- practice/cpp/Hackerrank/Cpp/BoxIt.cpp
+++ practice/cpp/Hackerrank/Cpp/BoxIt.cpp
@@ -79,47 +79,38 @@ ostream &operator<<(ostream &out, Box &B)
 };
 
 
-void check2()
-{
+void check2(){
 	int n;
 	cin>>n;
 	Box temp;
-	for(int i=0;i<n;i++)
-	{
+	for(int i=0;i<n;i++){
 		int type;
 		cin>>type;
-		if(type ==1)
-		{
+		if(type ==1){
 			cout << temp <<endl;
 		}
-		if(type == 2)
-		{
+		if(type == 2){
 			int l,b,h;
 			cin>>l>>b>>h;
 			Box NewBox(l,b,h);
 			temp=NewBox;
 			cout << temp <<endl;
 		}
-		if(type==3)
-		{
+		if(type==3){
 			int l,b,h;
 			cin>>l>>b>>h;
 			Box NewBox(l,b,h);
-			if(NewBox < temp)
-			{
+			if(NewBox < temp){
 				cout<<"Lesser\n";
 			}
-			else
-			{
+			else{
 				cout<<"Greater\n";
 			}
 		}
-		if(type==4)
-		{
+		if(type==4){
 			cout<<temp.CalculateVolume()<<endl;
 		}
-		if(type==5)
-		{
+		if(type==5){
 			Box NewBox(temp);
 			cout<<NewBox<<endl;
 		}
