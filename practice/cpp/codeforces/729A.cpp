5a737565eaeb9b3f733de5c494646982a8492fd1
manikanta2901 <manikanta2901@gmail.com>
2021-07-08 21:58:07 +0530

    Added one sol

---
+++ practice/cpp/codeforces/729A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        long long a,b;
+        cin >> a >> b;
+        if(a==b)
+            cout << 0 << " " << 0 << '\n';
+        else
+        {
+            long long g = abs(a-b);
+            long long m = min(a%g,g-a%g);
+            cout << g << " " << m << '\n';
+        }
+    }
+}
