cfc95f70454c3b910c8b620a28ee19af6515059e
manikanta2901 <manikanta2901@gmail.com>
2021-08-23 17:38:43 +0530

    Contiguous subarraySum Added

---
+++ random/ConSubarraySum.cpp
@@ -0,0 +1,41 @@
+// Contiguous SubArray Sum with given sum
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void SubarrayWithGivenSum(){
+    int n, s;
+    cin >> n >> s;
+    int a[n];
+    for (int i=0; i<n; i++)
+        cin >> a[i];
+    int i=0, j=0; int st=-1, en=-1; int sum = 0;
+    while(j<n && sum + a[j] <= s){
+        sum += a[j];
+        j++;
+    }
+    if (sum == s) {
+        cout << i + 1 << " "<< j << endl;
+        return;
+    }
+
+    while(j < n) {
+        sum += a[j];
+        while (sum > s){
+            sum -= a [i];
+            i++;
+        }
+        if (sum == s) {
+            st = i+1;
+            en = j+1;
+            break;
+        }
+    }
+
+    cout << st <<" "<< en <<  endl;
+}
+
+int main(){
+    SubarrayWithGivenSum();
+    return 0;
+}
